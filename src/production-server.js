9815106c72f5fca8928ceec1f749f4ad8b1e454d
dannycho7 <dannycho7@gmail.com>
2017-03-29 17:56:52 -0700

    Heroku deployment

---
+++ src/production-server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+var port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', _path2.default.join(__dirname, 'views'));
+
+//middleware for debugging requests
+app.use(function (req, res, next) {
+  console.log('URL Requested: ', req.url);
+  next();
+});
+
+app.use(_express2.default.static(_path2.default.join(__dirname, 'static')));
+
+// universal routing and rendering
+app.get('*', function (req, res) {
+  var titlePath = 'Danny\'s React App';
+  console.log('GET Request URL: ', req.url);
+  res.render('index', { path: titlePath });
+});
+
+app.listen(port, function () {
+  console.log('Server listening in on port ' + port);
+});
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
-var app = express();
-var port = ( process.env.PORT || 3000 );
+const app = express();
+const port = ( process.env.PORT || 3000 );
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
